7f0339615294252c007b376971d788187130be92
cobaltbluesky <kosentaguri1@gmail.com>
2020-01-07 02:27:58 +0900

    現在地の水色表示、コンタクトページの作成。

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const AboutMe=()=>{
+  return(
+    <div>
+      <Header/>
+      <Footer/>
+      <img src='./shizuku192.png'alt='IconBy荻素'/>
+      <h3>gunjou</h3>
+      <h3>久留米高専2年制御情報工学科</h3>
+      <h3>2003年3月16日生まれ</h3>
+    </div>
+  );
+}
+export default AboutMe;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Contact=()=>(
+  <div>
+    <Header/>
+      <p>準備中です。</p>
+    <Footer/>
+  </div>
+)
+export default Contact;
