5135c1569656639a5921a0332e4bda2fe3c39f2a
Gunjou <kosentaguri1@gmail.com>
2020-01-31 23:37:19 +0900

    a

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,6 +8,8 @@ const AboutMe=()=>{
       <img src='./shizuku192.png'alt='IconBy荻素'/>
       <h3>HN:gunjou</h3>
       <h3>本名:田栗青空</h3>
+      <h3>ほんみょう:たぐりせいあ</h3>
+      <h3>年齢:16歳</h3>
       <h3>学校:久留米高専</h3>
       <h3>学科:制御情報工学科</h3>
       <h3>学年:2年生</h3>
